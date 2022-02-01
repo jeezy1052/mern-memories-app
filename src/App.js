297ed87723985ee479d6f23d3d513c99fd835d9d
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-01-31 22:04:41 -0800

    before redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
 import memories  from '../src/images/memories.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+import useStyles from './styles'
 
 
 
 const App = () => {
+  const classes = useStyles(); 
+
+
   return (
     <Container maxWidth="lg">
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt='memories' height="360" />
+      <AppBar position='static' color='inherit' className={classes.appBar}>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={memories} alt='memories' height="60" />
       </AppBar>
       <Grow in>
         <Container>
