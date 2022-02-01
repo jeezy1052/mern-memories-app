5a3e5e7906366af96d4d7c79ca06b564423ddb0c
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-02-01 15:37:43 -0800

    d

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -8,6 +8,7 @@ export const getPosts = () => async (dispatch) => {
         dispatch({ type: 'FETCH_ALL', payload: data })
 
     } catch (error) {
+        
         console.log(error.message)
     }
 }
@@ -18,8 +19,8 @@ export const createPost = (post) => async (dispatch) => {
         const { data } = await api.createPost(post)
 
         dispatch({ type: 'CREATE', payload: data })
-
     } catch (error) {
+
         console.log(error)
     }
 }
@@ -30,6 +31,29 @@ export const  updatePost = (id, post) => async (dispatch) => {
 
        dispatch({ type: 'UPDATE', payload: data })
     } catch (error) {
+
+        console.log(error)
+    }
+}
+
+export const deletePost = (id) =>  async (dispatch) => {
+    try {
+        await api.deletePost(id)
+
+        dispatch({ type: 'DELETE', payload: id})
+    } catch (error) {
+
+        console.log(error)
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+       const { data } = await api.likePost(id);
+
+       dispatch({ type: 'UPDATE', payload: data })
+    } catch (error) {
+
         console.log(error)
     }
 }
